8f8dddb36984bc4896dbd8eacf162d984ecc099d
Daniel Carmo <daniel.carmo2008@gmail.com>
2023-01-15 00:08:47 -0400

    add: cart and toolbar

---
+++ src/app/store/store.component.ts
@@ -0,0 +1,45 @@
+import { map, Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { StoreService } from './services/store.service';
+import { IProduct } from './models/store.interface';
+
+@Component({
+  selector: 'store',
+  templateUrl: './store.component.html',
+  styleUrls: ['./store.component.scss'],
+})
+export class StoreComponent implements OnInit {
+  public products$: Observable<IProduct[]> = new Observable<[]>();
+  public cartItems: IProduct[] = [];
+  emptyCart = true;
+
+  constructor(private storeService: StoreService) {}
+
+  ngOnInit(): void {
+    this.products$ = this.storeService
+      .getProducts()
+      .pipe(map((products) => products));
+  }
+
+  openCart() {}
+
+  addToCard(item: IProduct) {
+    this.addItem(item);
+  }
+
+  addItem(item: IProduct) {
+    this.cartItems.push(item);
+    console.log(this.cartItems);
+  }
+
+  deleteItem(id: number) {
+    if (this.cartItems.length > 0)
+      this.cartItems.forEach((item: IProduct, index) => {
+        if (item.id === id) this.cartItems.splice(index, 1);
+      });
+  }
+
+  checkEmptyCart() {
+    return !(this.cartItems.length > 0);
+  }
+}
